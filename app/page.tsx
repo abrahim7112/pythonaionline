612dfd59fd5ef0cf279df63b9cc74155785c07e1
ibrahim ali <82097150+abrahim7112@users.noreply.github.com>
2024-12-29 01:38:11 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
+  
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
@@ -31,22 +31,28 @@ export default function App() {
     });
   }
 
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+  
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
+        {todos.map(todo => <li
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>
+          {todo.content}
+        </li>)}
       </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
+        <a href="https://docs.amplify.aws/react/start/quickstart/">Review next step of this tutorial.</a>
       </div>
+      <button onClick={createTodo}>Sign out</button>
     </main>
   );
 }
